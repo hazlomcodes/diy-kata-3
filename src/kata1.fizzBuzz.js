628e8430a21d54e3bdff8dd7704e1fcc95a33c82
hazlomcodes <harry.lomas1@outlook.com>
2022-10-11 16:02:02 +0100

    fizzbuzz exercises complete

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,20 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+    const isDivisableBy3 = (number % 3) === 0
+    const isDivisableby5 = (number % 5) === 0
+    
+    if (isDivisableBy3 && isDivisableby5) {
+        return 'FizzBuzz'
+    }
+
+  if (isDivisableBy3) {
+    return 'Fizz'
+  }
+
+  if (isDivisableby5){
+    return 'Buzz'
+  }
+
+  return number
+};
 
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,16 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it('returns Fizz when passed a multiple of 3',() => {
+    expect(fizzBuzz(3)).toBe('Fizz')
+        
+    })
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('Buzz')
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {});
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns the number when it isn't a multiple of 3 or 5", () => {});
 });
