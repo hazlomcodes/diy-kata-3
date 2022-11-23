7002847783a601f763f1dd064118ef1233a9fd50
hazlomcodes <harry.lomas1@outlook.com>
2022-11-23 15:31:20 +0000

    passed test 5

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const estimatedTime = Math.round((distance / speed) * 2) / 2;
+    return `I should be there in ${estimatedTime} hours.`;
+  };
+  
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,10 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(90, 45)).toEqual("I should be there in 2 hours.");
+  });
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(30, 63)).toEqual("I should be there in 0.5 hours.");
+  });
 });
