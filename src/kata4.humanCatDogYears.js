0b52b7afbd6a445e66c909c3239e2ecebfd51fa7
hazlomcodes <harry.lomas1@outlook.com>
2022-11-23 15:29:06 +0000

    better late than never test 4 passed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,19 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+    let catYears = 0;
+    let dogYears = 0;
+    for (let i = 0; i < number; i++) {
+      if (i === 0) {
+        catYears += 15;
+        dogYears += 15;
+      } else if (i === 1) {
+        catYears += 9;
+        dogYears += 9;
+      } else {
+        catYears += 4;
+        dogYears += 5;
+      }
+    }
+    return [number, catYears, dogYears];
+  };
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    it("represents human years to cats and dogs", () => {
+     expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+    });
+    it("represents human years to cats and dogs", () => {
+      expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    });
+  });
